39b44603393e120e4e55487b512f007ac3f13310
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-08-26 16:13:20 +0200

    ul riempita

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 var listaCognomi = ["Bianchi","Neri", "Rossi", "Verdi", "Gialli"];
 
-var nuovoCognome = prompt("Congome: ");
+var nuovoCognome = prompt("Cognome: ");
 
 listaCognomi.push(nuovoCognome);
 
 listaCognomi.sort();
+let a=document.getElementsByClassName("cognome");
 
-for(let i=1;i<listaCognomi.length;i++){
-    console.log(i + ". " + listaCognomi[i]);
+for(let i=0;i<listaCognomi.length;i++){
+    a[i].textContent = i+1 + ". " + listaCognomi[i];
 }
+
